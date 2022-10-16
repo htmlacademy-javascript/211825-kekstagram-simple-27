cdedc38d32ee508a7c2edcb2905d41ae1b0a4276
KSait0 <6wayofpain@gmail.com>
2022-10-16 15:05:37 +0300

    Написал функции для генерации данных

--- js/main.js
+++ js/main.js
@@ -13,4 +13,42 @@ function checkStringLength (string, length) {
 }
 
 getRandomNumber();
-checkStringLength();
+checkStringLength('d', 1);
+
+const OBJECT_AMOUNT = 25;
+
+const DESCRIPTION = [
+  'My first photo',
+  'Всем привет!',
+  'А это я',
+  'Я и мои чюваки',
+  'Вот так вот',
+  'Эта фотка из самого классного места',
+  'Обычно не получается, но тут получилось',
+  'Как вам? ',
+  'Лучшее из того что наснимали',
+  'Как то так получилось',
+];
+
+function createSingleObject (id) {
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: DESCRIPTION[getRandomNumber(0, DESCRIPTION.length - 1)],
+    likes: getRandomNumber(15, 200),
+    comments: getRandomNumber(0, 200),
+  };
+}
+
+
+function createMockData (amount) {
+  const photos = [];
+  for (let i = 1; i <= amount; i++) {
+    photos.push(createSingleObject(i));
+  }
+  return photos;
+}
+
+createMockData(OBJECT_AMOUNT);
+
+
