6021b6bb56ce714538671b3c4b1b156acf24c444
KSait0 <6wayofpain@gmail.com>
2022-10-23 15:09:44 +0300

    Разделил код на модули

---
+++ js/mock-data.js
@@ -0,0 +1,39 @@
+import { getRandomNumber } from './util.js';
+
+const OBJECT_AMOUNT = 25;
+
+const DESCRIPTION = [
+  'My first photo',
+  'Всем привет!',
+  'А это я',
+  'Я и мои чюваки',
+  'Вот так вот',
+  'Эта фотка из самого классного места',
+  'Обычно не получается, но тут получилось',
+  'Как вам? ',
+  'Лучшее из того что наснимали',
+  'Как то так получилось',
+];
+
+function createSingleObject (id) {
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: DESCRIPTION[getRandomNumber(0, DESCRIPTION.length - 1)],
+    likes: getRandomNumber(15, 200),
+    comments: getRandomNumber(0, 200),
+  };
+}
+
+
+function createMockData (amount) {
+  const photos = [];
+  for (let i = 1; i <= amount; i++) {
+    photos.push(createSingleObject(i));
+  }
+  return photos;
+}
+
+const allPhotos = createMockData(OBJECT_AMOUNT);
+
+export { allPhotos };
